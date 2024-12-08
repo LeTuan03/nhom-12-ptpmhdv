3bde0700d8bda6819351fb73ce9abc327edfa8dc
LeTuan <leetuaans03@gmail.com>
2024-12-09 01:43:48 +0700

    feat/17: BE - Viết api tìm kiếm địa điểm

--- nhom12-be/src/main/java/com/_cn4/nhom12/repository/DestinationRepo.java
+++ nhom12-be/src/main/java/com/_cn4/nhom12/repository/DestinationRepo.java
@@ -2,6 +2,22 @@ package com._cn4.nhom12.repository;
 
 import com._cn4.nhom12.entity.Destination;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface DestinationRepo extends JpaRepository<Destination, String > {
+import java.util.List;
+
+public interface DestinationRepo extends JpaRepository<Destination, String> {
+    @Query("SELECT d FROM Destination d WHERE " +
+            "(:destinationName IS NULL OR LOWER(d.name) LIKE LOWER(CONCAT('%', :destinationName, '%'))) AND " +
+            "(:cityName IS NULL OR LOWER(d.city.name) LIKE LOWER(CONCAT('%', :cityName, '%'))) AND " +
+            "(:countryName IS NULL OR LOWER(d.country.name) LIKE LOWER(CONCAT('%', :countryName, '%'))) AND " +
+            "(:continentName IS NULL OR LOWER(d.continent.name) LIKE LOWER(CONCAT('%', :continentName, '%')))")
+    List<Destination> search(
+            @Param("destinationName") String destinationName,
+            @Param("cityName") String cityName,
+            @Param("countryName") String countryName,
+            @Param("continentName") String continentName
+    );
+
 }
